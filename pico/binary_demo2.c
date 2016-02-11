068dd2a814f1bfcd1e0394e9ec3f653a7435ecdb
alexwh <alexwh@users.noreply.github.com>
2016-02-11 15:52:25 +0000

    oops

--- pico/binary_demo2.c
+++ pico/binary_demo2.c
@@ -1,7 +1,7 @@
-include <stdlib.h>
-include <stdio.h>
-include <string.h>
-include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   setbuf(stdin, NULL); // Don't worry about this.
