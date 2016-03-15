913c17f56713d083437030370680edc561b31328
alexwh <alexwh@users.noreply.github.com>
2016-03-15 02:44:57 +0000

    ok github I'll wrap to 80 cols if you don't go red

--- hackyeaster/palm/wow.js
+++ hackyeaster/palm/wow.js
@@ -43,7 +43,8 @@ eval(atob("ZXZhbChmdW5jdGlvbihwLGEsYyxrLGUsZCl7ZT1mdW5jdGlvbihjKXtyZXR1cm4gYy50b
 // which decodes to:
 eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('r q(){2 u=7.8(\'w\').b;2 p=7.8(\'v\').b;2 4=[0,0,0,0,0,0,0,0,0,0];2 5=c;3(u===\'g\'){3(p>0&&p.h==9){5=j;f(i=1;i<=9;i++){2 6=p.k(i-1);2 a=p.e(0,i);3(4[6]!=0||a%i!=0){5=c}3(4[6]==0){4[6]=1}}}}3(5){7.m.n=\'o\'+u+\'x\'+p+\'.s\'}t{d(\'l\')}}',34,34,'||var|if|used|ok|digit|document|getElementById|10|part|value|false|alert|substring|for|elsa|length||true|charAt|nope|location|href|challenge14_||checkEntries|function|html|else||ppass|puser|_'.split('|'),0,{}))"
 
-// didn't fancy that, so I set a breakpoint in chrome debugger tools to get the deobfuscated result (beautified):
+// didn't fancy that, so I set a breakpoint in chrome debugger tools to get the
+// deobfuscated result (beautified):
 
 function checkEntries() {
     var u = document.getElementById('puser').value;
