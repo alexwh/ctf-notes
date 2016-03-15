0aae6b5773ab959f7b57891e4cb1ac5c0067843a
alexwh <alexwh@users.noreply.github.com>
2016-03-15 01:35:21 +0000

    finished solution
    had to run this shit in a vm with VS on. please don't make me again

--- hackyeaster/bug_hunter/DeggCryptor/KeyGen.cs
+++ hackyeaster/bug_hunter/DeggCryptor/KeyGen.cs
@@ -16,10 +16,10 @@ namespace DeggCryptor
         public static string generateKey()
         {
             // Init the four seed values. 1111 and multiples of it.
-            int h1 = 0x1111;
-            int h2 = 0x2222;
-            int h3 = 0x3333;
-            int h4 = 0x4444;
+            int h1 = 1111;
+            int h2 = 2222;
+            int h3 = 3333;
+            int h4 = 4444;
 
             // Init variables.
             int a = h1;
@@ -28,28 +28,30 @@ namespace DeggCryptor
             int d = h4;
 
             // 1'000 iterations.
-            for (int i = 1; i < 1000; i++)
+            for (int i = 1; i <= 1000; i++)
             {
                 // If c is greater than d, double c and d.
                 if (c > d)
+                {
                     c *= 2;
                     d *= 2;
+                }
 
                 // Calculate new values.
                 // a: Take sum of a and b, and c and d. Then, multiply the two values.
-                a = a + b * c + d;
+                a = (a + b) * (c + d);
                 // b: multiply with 3. Using two additions instead of multiplying -> performance booooost!
-                b = b + b;
-                b = b + b;
+                b *= 3;
                 // c, d: Swap c and d 
+                int temp = c;
                 c = d;
-                d = c;
+                d = temp;
 
                 // Take last four digits (modulo 10'000),
                 a %= 10000;
                 b %= 10000;
                 c %= 10000;
-                d %= 100000;
+                d %= 10000;
             }
             // Password: a,b,c and d with "X" in between
             string password = a + "X" + b + "X" + c + "X" + d;
